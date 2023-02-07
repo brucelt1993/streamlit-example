5c2b7c9d3441213509452ea317c82094c30a4ac9
brucelt1993 <brucelt_1993@163.com>
2023-02-07 10:10:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,33 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import requests
+import re
+from bs4 import BeautifulSoup
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+def extract_images(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    images = []
+    for img in soup.find_all("img"):
+        src = img.attrs.get("src")
+        if src:
+            src = re.sub("^//", "https://", src)
+            images.append(src)
+    return images
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def main():
+    st.title("图片提取器")
+    url = st.text_input("输入url:")
+    if st.button("提取链接"):
+        images = extract_images(url)
+        if not images:
+            st.error("当前页面无图片")
+        else:
+            st.success("发现 {} 张图片.".format(len(images)))
+            for i, image in enumerate(images):
+                st.write("Image {}:".format(i + 1))
+                st.write("URL: {}".format(image))
+                st.write("Download:")
+                st.markdown("[Download]({})".format(image))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
